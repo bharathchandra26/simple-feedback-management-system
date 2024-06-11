46b4200c95d0c8b9ec87bfb3e92b0f2deefd046f
bharathchandra26 <141152796+bharathchandra26@users.noreply.github.com>
2024-06-11 18:49:27 +0530

    feedback.service.ts

--- backend/src/services/feedback.service.ts
+++ backend/src/services/feedback.service.ts
@@ -1,1 +1,12 @@
+interface Feedback {
+  name: string;
+  feedback: string;
+}
 
+let feedbacks: Feedback[] = [];
+
+export const getAllFeedbacks = (): Feedback[] => feedbacks;
+
+export const addFeedback = (name: string, feedback: string): void => {
+  feedbacks.push({ name, feedback });
+};
